f08fe56a2d76584f4bedaec03fec8ae0f67ca3b0
Rėjus_M <rejusm@gmail.com>
2024-05-28 10:53:30 +0300

    Startas

---
+++ sportodb/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for sportodb project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sportodb.settings")
+
+application = get_wsgi_application()
