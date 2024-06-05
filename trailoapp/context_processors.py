3556563ba02c656e626ba6d859ef0b910cb6be90
Rėjus_M <rejusm@gmail.com>
2024-06-05 09:29:12 +0300

    nav baro klaida sutvarkyta

--- trailoapp/context_processors.py
+++ trailoapp/context_processors.py
@@ -2,5 +2,5 @@ from .models import Stage
 
 
 def stages_processor(request):
-    stages = Stage.objects.all()
-    return {'stages': stages}
+    stagess = Stage.objects.all()
+    return {'stagess': stagess}
